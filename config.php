bc86319c375273d18759048818967b976119965f
luis <luisnavarroFS@gmail.com>
2024-05-01 17:43:18 +0200

    feat: add .env

--- config.php
+++ config.php
@@ -7,4 +7,11 @@ use eftec\bladeone\BladeOne;
 $views = __DIR__ . '/views';
 $cache = __DIR__ . '/cache';
 $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG); 
+
+$env = parse_ini_file('.env');
+$conn = new mysqli($env['SERVER'], $env['USER'], $env['PASS'], $env['DB']);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
